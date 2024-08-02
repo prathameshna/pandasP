8c38ec52f7f96133a7414b9e077a7f3c1ec3e79d
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-08-02 09:26:52 +0530

    first commit

--- whileLoop.py
+++ whileLoop.py
@@ -1,6 +1,5 @@
-# i = int(input("Enter the Number: "))
-# while(i<=3):
-#     print(i)
-#     i = i + 1
-#     print("Done the loop")
-
+i = int(input("Enter the Number: "))
+while(i<=3):
+    print(i)
+    i = i + 1
+    print("Done the loop")
