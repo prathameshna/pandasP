72cfad077aad9739b5fdb6c59de06e116f3df370
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-08-25 08:41:05 +0530

    first commit

--- cheatSheet🛣️.py
+++ cheatSheet🛣️.py
@@ -2,3 +2,4 @@
 # Tuples are immutable.
 # Tuples are immutable, hence if you want to add , remove or change tuple items, then first you must convert the tuple to a list. then perform operation on that list and convert it back to tuple.
 # list are mutable.
+# RECURSION:- In a function, we call the same function that is called recursion.
